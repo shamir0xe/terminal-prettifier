9177ec367ea6cad930a599590e398027b910b734
shamir0xe <shamir0xe@gmail.com>
2023-08-17 01:48:38 +0330

    fix: change color codes, change user.config too

--- src/mediators/stream_mediator.py
+++ src/mediators/stream_mediator.py
@@ -22,15 +22,6 @@ class StreamMediator:
         return self
 
     def print_word(self, word: str, colors_arr: List[Colors]) -> None:
-        # print("\033[1;37;40m \033[2;37:40m TextColour BlackBackground          TextColour GreyBackground                WhiteText ColouredBackground\033[0;37;40m\n")
-        # print("\033[1;30;40m Dark Gray      \033[0m 1;30;40m            \033[0;30;47m Black      \033[0m 0;30;47m               \033[0;37;41m Black      \033[0m 0;37;41m")
-        # print("\033[1;31;40m Bright Red     \033[0m 1;31;40m            \033[0;31;47m Red        \033[0m 0;31;47m               \033[0;37;42m Black      \033[0m 0;37;42m")
-        # print("\033[1;32;40m Bright Green   \033[0m 1;32;40m            \033[0;32;47m Green      \033[0m 0;32;47m               \033[0;37;43m Black      \033[0m 0;37;43m")
-        # print("\033[1;33;40m Yellow         \033[0m 1;33;40m            \033[0;33;47m Brown      \033[0m 0;33;47m               \033[0;37;44m Black      \033[0m 0;37;44m")
-        # print("\033[1;34;40m Bright Blue    \033[0m 1;34;40m            \033[0;34;47m Blue       \033[0m 0;34;47m               \033[0;37;45m Black      \033[0m 0;37;45m")
-        # print("\033[1;35;40m Bright Magenta \033[0m 1;35;40m            \033[0;35;47m Magenta    \033[0m 0;35;47m               \033[0;37;46m Black      \033[0m 0;37;46m")
-        # print("\033[1;36;40m Bright Cyan    \033[0m 1;36;40m            \033[0;36;47m Cyan       \033[0m 0;36;47m               \033[0;37;47m Black      \033[0m 0;37;47m")
-        # print("\033[1;37;40m White          \033[0m 1;37;40m            \033[0;37;40m Light Grey \033[0m 0;37;40m               \033[0;37;48m Black      \033[0m 0;37;48m")
         new_word = reduce(
             lambda a, b: a + b,
             map(
@@ -49,6 +40,8 @@ class StreamMediator:
             while self.ta.check_escape(self.reader):
                 self.raw_print(self.reader.next_char())
             word = self.reader.next_string()
+            if word == '':
+                continue
             if self.ta.check_edits(word):
                 self.raw_print(word)
                 continue
--- src/types/colors.py
+++ src/types/colors.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 
 class Colors(Enum):
-    BLACK = "\033[1;30;40m"
-    RED = "\033[1;31;40m"
-    GREEN = "\033[1;32;40m"
-    YELLOW = "\033[1;33;40m"
-    BLUE = "\033[1;34;40m"
-    MAGENTA = "\033[1;35;40m"
-    CYAN = "\033[1;36;40m"
-    WHITE = "\033[1;37;40m"
+    BLACK = "\033[1;30;30m"
+    RED = "\033[1;31;31m"
+    GREEN = "\033[1;32;32m"
+    YELLOW = "\033[1;33;33m"
+    BLUE = "\033[1;34;34m"
+    MAGENTA = "\033[1;35;35m"
+    CYAN = "\033[1;36;36m"
+    WHITE = "\033[1;37;37m"
     RESET = "\033[0m"
